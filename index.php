4d2bab0e83eb13050ca1f183a51bd6fefb594bc8
GianlucaSpendolini <alpagi01@gmail.com>
2024-02-14 16:36:50 +0100

    cambio nome index.php -g

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+        <title>index</title>
+    </head>
+    <body>
+        <h1>>Hello World</h1>
+        <p>I'm hosted with GitHub Pages.</p>
+        <h4> Provo ad inserire le cose</h4>
+        <a href="https://www.google.com/"> Google.com </a>
+        <br/>
+        <a href="script.php"> PHP </a>
+        <?php
+            $ciao = "Ciao";
+
+            echo $ciao;
+            ?>
+    </body>
+</html>
