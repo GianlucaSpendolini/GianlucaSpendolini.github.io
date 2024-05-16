5a4314a04690c6600f80a3323b4a8fb679f0d4e8
GianlucaSpendolini <alpagi01@gmail.com>
2024-05-16 22:03:19 +0200

    provo ad aggiungere onclick tramite js -g

--- Static/javascript/prova.js
+++ Static/javascript/prova.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Per controllo se funziona
     console.log('ciaoooo funziona');
+    alert("boh");
+
+    document.querySelector('#buttProva').addEventListener('click', prova("ciaooooo"));
 });
 
 
-function prova() {
-    console.log("ciaoooo");
+function prova(messaggio) {
+    console.log(messaggio);
 }
