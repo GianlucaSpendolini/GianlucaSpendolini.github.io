6f0b2a1a21f4fcecec5afd13acd973c5777acc45
GianlucaSpendolini <alpagi01@gmail.com>
2024-06-02 19:35:50 +0200

    commit file generico scripts -g

---
+++ Static/javascript/scripts.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Cose da fare inizialmente (inserire connessioni nei bottoni)
+
+    // In base alla sezione in cui mi trovo, faccio qualcosa
+    setTimeout(what_page(window.location.pathname), 0);
+});
+
+
+function what_page(path) {
+    // Prova
+    if (path === '/prova') {
+        alert('Ciao prova');
+    }
+
+    // About
+    if (path === '/about') {
+        const date = new Date();
+        const my_age = data.getFullYear() - 2001;
+
+        if (date.getDate() < 11 || (date.getMonth() + 1) < 9) {
+            my_age -= 1;
+        }
+        // Inserisco il mio anno nella presentazione
+        document.querySelector('#my-age').innerHTML = my_age;
+    }
+}
