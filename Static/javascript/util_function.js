803165bf0e613e66167d0ffcc02d0d088f457c40
GianlucaSpendolini <alpagi01@gmail.com>
2025-03-12 17:25:13 +0100

    correzione errore di distrazione -g
    aggiunto riferimento a #tornare-alla-home

--- Static/javascript/util_function.js
+++ Static/javascript/util_function.js
@@ -440,7 +440,7 @@ export function insert_examples(reason, script_name) {
                     
                     // Versione PHP
                    
-                    divs.push(document.querySelector('#'));
+                    divs.push(document.querySelector('#tornare-alla-home'));
 
                     html_examples.push({
                         'JavaScript': `/* Non ci sono codici di esempio da eseguire*/`,
