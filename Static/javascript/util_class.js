99938f96c2cd748c948aa0faa3d5511e9abb0a3e
GianlucaSpendolini <alpagi01@gmail.com>
2025-04-04 11:23:46 +0200

    controllo se è un array -g
    metto una funzione asincrona dentro convert() perchè altrimenti mi restituisce una promise e non l'oggetto in se

--- Static/javascript/util_class.js
+++ Static/javascript/util_class.js
@@ -31,26 +31,29 @@ export class UserText {
     /*
         Conversioni
     */
-    async convert(to) {
+    convert(to) {
 
-        // Richiamo il file JSON
-        await fetch(`${points_number(path)}Static/json/encoding-map.json`)
-        .then(response => response.json())
-        .then(data => {
+        (async () => {
 
-            // // Prendo i dati che mi servono
-            // let array = data[to];
-            this.chars = data[to];
+            // Richiamo il file JSON
+            await fetch(`${points_number(path)}Static/json/encoding-map.json`)
+            .then(response => response.json())
+            .then(data => {
+    
+                // // Prendo i dati che mi servono
+                // let array = data[to];
+                this.chars = data[to];
+    
+                // // Controllo se è un array -> se non: trasformo (prendo solo valori dell'oggetto ['num': 'array'])
+                // if (!Array.isArray(array)) {
+                //     this.chars = Object.values(array);
+                // }
+                this.prova = [data, to, this.chars];
+    
+            });
 
-            // // Controllo se è un array -> se non: trasformo (prendo solo valori dell'oggetto ['num': 'array'])
-            // if (!Array.isArray(array)) {
-            //     this.chars = Object.values(array);
-            // }
-            this.prova = [data, to, this.chars];
-
-        });
-
-        return this;
+            return this;
+        })();
         
     }
 
