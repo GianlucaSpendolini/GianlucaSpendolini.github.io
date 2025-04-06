f8411bf67a9be7099486f66d5019a123cee67898
GianlucaSpendolini <alpagi01@gmail.com>
2025-04-06 22:11:14 +0200

    vedo cosa passa nella condizione se morse -g
    controllo cosa viene assegnato a char quando provo ad assegnare altro x2

--- Static/javascript/util_class.js
+++ Static/javascript/util_class.js
@@ -156,11 +156,11 @@ export class UserText {
                             }
 
                             // PROVA
-                            converted_char.push(``);
+                            converted_char.push(`fuori: ${char}`);
                             // Venendo dal testo -> faccio una conversione semplice (in base alla chiave trovo il valore)
                             if (Object.keys(group).includes(char)) {
                                 // PROVA
-                                converted_char.push('dentro');
+                                converted_char.push(`dentro: ${char}`);
                                 // PROVA
                                 converted_char.push(group[char]);
                                 // converted_char = group[char];
