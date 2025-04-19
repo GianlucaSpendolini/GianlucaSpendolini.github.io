3d8d14da8d9eb9e2884820c0e1b70cd5f54c36c8
GianlucaSpendolini <alpagi01@gmail.com>
2025-04-19 23:56:20 +0200

    replace dentro metodo find della classe -g
    provo ad inserire la classe ma anche di default quando inizializzo la variabile nel metodo

--- Static/javascript/util_class.js
+++ Static/javascript/util_class.js
@@ -230,10 +230,10 @@ export class UserText {
         - Se cerco per sostituire -> vado a sostituire
             - Se ho selezionato anche isRegex non c'è alcuna differenza
     */
-    find(pattern, isRegex=false, toReplace=null) {
+    find(pattern, isRegex=false, toReplace={'?': false}) {
 
         // Se devo sostituire
-        if (toReplace && toReplace['?']) {
+        if (toReplace['?']) {
             return this.text.replace(isRegex ? new RegExp(pattern, 'g') : pattern, toReplace['replacement']);
         }
 
