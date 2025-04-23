8339f8a0355cec57144b1d99396f9555a13f54e0
GianlucaSpendolini <alpagi01@gmail.com>
2025-04-23 09:39:53 +0200

    sistemata proporzionalità del campo password -g
    ora il campo password è più grande quando si usano schermi di massima larghezza 360px + spostato ridimensionamento dell'header (con incolonnamento dei bottoni sulla dx) da max 319px a 360px (in modo da non aspettare che esca dalla centratura spostandosi a dx ma si ridimensiona quando è quasi al limite di spazio)

--- Static/javascript/scripts.js
+++ Static/javascript/scripts.js
@@ -508,7 +508,7 @@ function what_page(points_path) {
             break;
 
         // /password-checker
-        case '/utilities/password-checker':
+        case '/utilities/password-checker.html':
 
             // Titolo
             title.innerHTML = 'Password checker';
